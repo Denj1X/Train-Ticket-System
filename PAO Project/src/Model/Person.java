40008f1af8b24848a026609810a454972294d577
Matei Biciușcă <white.hollow.ro@gmail.com>
2023-04-20 13:11:22 +0300

    small update

---
+++ PAO Project/src/Model/Person.java
@@ -0,0 +1,38 @@
+package Model;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private Integer age;
+
+    public Person(String _firstName, String _lastName, Integer _age) {
+        super();
+        this.firstName = _firstName;
+        this.lastName = _lastName;
+        this.age = _age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
--- PAO Project/src/Model/Seat.java
+++ PAO Project/src/Model/Seat.java
@@ -1,4 +1,67 @@
 package Model;
 
 public class Seat {
+    private int seatNumber;
+    private boolean available;
+    private Passenger passenger;
+    protected double fare;
+
+    public Seat(int seatNumber) {
+        this.seatNumber = seatNumber;
+        this.available = available;
+        this.fare = fare;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Passenger passenger) {
+        this.passenger = passenger;
+    }
+
+    public double getFare() {
+        return fare;
+    }
+
+    public void setFare(double fare) {
+        this.fare = fare;
+    }
+
+    public boolean reserveSeat() {
+        if (available) {
+            available = false;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean cancelSeat() {
+        if (!available) {
+            available = true;
+            passenger = null;
+            return true;
+        }
+        return false;
+    }
+
+    public Passenger getReservedPassenger() {
+        return passenger;
+    }
 }
